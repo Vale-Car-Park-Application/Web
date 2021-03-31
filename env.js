e7e5aec5dc154223bd32c685b78e3358d128a55c
femresirvan <femresirvan@gmail.com>
2021-03-31 03:03:47 +0300

    env.js kaldırılamama bug ı giderildi

--- env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: 3000,
-    CLIENT_ID: "160339418084-vlh8ompn97pktef4gnlfs12b59jrhblv.apps.googleusercontent.com",
-    CLIENT_SECRET: "EPFSR0c8I9IAJTZG6PL3J-Oc",
-    MONGO_URI: 'mongodb+srv://femresirvan:147852369Fee@cluster0.4ubsi.mongodb.net/ieeeiot?retryWrites=true&w=majority'
-}
